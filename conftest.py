bedebb28218337db06ee53a98a624d7a883f60bd
Andrey <sidorov.avk@gmail.com>
2020-09-29 09:37:36 +0400

    change DC

--- conftest.py
+++ conftest.py
@@ -11,10 +11,11 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities={'browserName': 'chrome', 'javascriptEnabled': True})
+    capabilities = DesiredCapabilities.CHROME
+    remote_server = 'http://localhost:4444/wd/hub'
+    browser = webdriver.Remote(remote_server, capabilities)
 
 
     browser.maximize_window()
