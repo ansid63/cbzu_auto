d35d4eae988c065b5c4c6cc06e448399ce2a7ba7
Andrey <sidorov.avk@gmail.com>
2020-09-29 13:19:38 +0400

    add

--- conftest.py
+++ conftest.py
@@ -13,9 +13,11 @@ def browser():
     # language = request.config.getoption("language")
     # options = Options()
     # options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    capabilities = DesiredCapabilities.FIREFOX
-    remote_server = 'http://192.168.170.14:4445/wd/hub'
-    browser = webdriver.Remote(remote_server, capabilities)
+    # capabilities = DesiredCapabilities.FIREFOX
+    # remote_server = 'http://192.168.170.14:4445/wd/hub'
+    browser = webdriver.Remote(command_executor='http://192.168.170.14:4445/wd/hub', desired_capabilities={'browserName': 'firefox', 'platform': 'LINUX'})
+    browser = webdriver.Remote(command_executor='localhost:4444/wd/hub',
+                               desired_capabilities={'browserName': 'firefox'})
 
     browser.maximize_window()
 
