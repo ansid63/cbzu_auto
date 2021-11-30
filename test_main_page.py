d645ebd4bbf8637202b4cf96890c4fba1d7a9ef9
Andrey Sidorov <andrey.sidorov@simbirsoft.com>
2021-11-30 16:41:46 +0400

    update test

--- test_main_page.py
+++ test_main_page.py
@@ -4,36 +4,33 @@ from .pages.main_page import MainPage
 import time
 
 
-@allure.title("Позитивный тест расчёта КБЖУ")
-def test_positive_value(browser):
-    new_user = MainPage(browser)
-    new_user.open()
+class TestCbzu:
+    @allure.title("Позитивный тест расчёта КБЖУ")
+    def test_positive_value(self, browser):
+        new_user = MainPage(browser)
+        new_user.open()
 
-    # input info about user
-    new_user.fill_value_positive()
-    time.sleep(5)
+        # input info about user
+        new_user.fill_value_positive()
+        time.sleep(5)
 
-    # check field
-    new_user.check_numbers()
-
-
-@allure.title("Негативный тест расчёта КБЖУ")
-def test_negative_value(browser):
-    new_user = MainPage(browser)
-    new_user.open()
-
-    # input info about user
-    new_user.fill_value_negative()
-    time.sleep(5)
-
-    # check field
-    try:
+        # check field
         new_user.check_numbers()
-    except NoSuchElementException:
-        print('Негативные данные не прошли')
-    finally:
-        print('Корректный негативный тест')
 
 
+    @allure.title("Негативный тест расчёта КБЖУ")
+    def test_negative_value(self, browser):
+        new_user = MainPage(browser)
+        new_user.open()
 
+        # input info about user
+        new_user.fill_value_negative()
+        time.sleep(5)
 
+        # check field
+        try:
+            new_user.check_numbers()
+        except NoSuchElementException:
+            print('Негативные данные не прошли')
+        finally:
+            print('Корректный негативный тест')
