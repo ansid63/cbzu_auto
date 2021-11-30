612ab1e6137114d52f16af9b0ab8c3570a41c57f
Andrey Sidorov <andrey.sidorov@simbirsoft.com>
2021-11-30 21:51:23 +0400

    add develop

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 @pytest.fixture(scope="function")
 def browser():
-    # browser = webdriver.Chrome()  # for local launch
+    # browser = webdriver.Chrome()  # for local launch obj
 
     chrome_options = webdriver.ChromeOptions()
     browser = webdriver.Remote(
